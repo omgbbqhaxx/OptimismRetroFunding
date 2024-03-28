8390ed18bd756bccc3400fc5ffdc98c6fc6932a5
yasin aktimur <yasinaktimur@gmail.com>
2024-03-29 00:37:11 +0300

    v2.

--- op.sol
+++ op.sol
@@ -2,18 +2,19 @@
 pragma solidity ^0.8.25;
 contract JammySwap  {
 
-
+  
   uint256 public minbetAM  = 10**13; //0.00001
   uint256 public maxbetAM = 1*(10**17); // thats means 1ETH
+  uint256 public winrate = 90;
 
-
+ 
   address payable private manager;
   uint256 private lpb = block.number;
   mapping(uint256 => address payable[] ) blockBets;
   mapping (string => uint256 ) private balances;
   mapping (uint256 => bool) private isuserwinner;
 
-
+  
 
 
 
@@ -49,17 +50,17 @@ contract JammySwap  {
          return append(uintToString(nAddrHash(_address)), uintToString(bnum));
     }
 
-
+    
 
     receive() external payable {
          if(msg.sender != manager) {
-
+        
             if(msg.value <= maxbetAM && msg.value >= minbetAM) {
               if(lpb != block.number) { for (uint256 i=0; i < blockBets[lpb].length; i++) { cashBack(lpb, blockBets[lpb][i]);}lpb = block.number; }
-
+             
                 blockBets[lpb].push(payable(msg.sender));
                 if(balances[MixAddrandSpBlock(lpb, msg.sender)] == 0) { balances[MixAddrandSpBlock(lpb, msg.sender)] = msg.value; }
-
+               
             } else {revert();}
          }
      }
@@ -67,18 +68,18 @@ contract JammySwap  {
 
      function test() public payable {
          if(msg.sender != manager) {
-
+        
             if(msg.value <= maxbetAM && msg.value >= minbetAM) {
               if(lpb != block.number) { for (uint256 i=0; i < blockBets[lpb].length; i++) { cashBack(lpb, blockBets[lpb][i]);}lpb = block.number; }
-
+             
                 blockBets[lpb].push(payable(msg.sender));
                 if(balances[MixAddrandSpBlock(lpb, msg.sender)] == 0) { balances[MixAddrandSpBlock(lpb, msg.sender)] = msg.value; }
-
+               
             } else {revert();}
          }
      }
 
-
+ 
     function checkReward(uint256 _bnum) private view returns(bool) {
          if(((block.number - 1) - _bnum) < 200) { //this ifblock ckeck 255 block hash problem.
              uint pb = uint256(blockhash(_bnum+1))%2; // future block hash
@@ -96,12 +97,12 @@ contract JammySwap  {
      if(balances[MixAddrandSpBlock(bnum, _addr)] != 0 && checkReward(bnum) == true) {
            uint256 usernewbalance = balances[MixAddrandSpBlock(bnum, _addr)] * 2 ;
            usernewbalance = usernewbalance/ 100;
-           _addr.transfer(usernewbalance * 80);
+           _addr.transfer(usernewbalance * winrate);
            balances[MixAddrandSpBlock(bnum, _addr)] = 0;
-     }
+     }  
   }
 
-
+    
 
   function setmaxPrice(uint256 newPrice) public {
     require(msg.sender == manager,"only manager can reach  here");
@@ -111,6 +112,11 @@ contract JammySwap  {
   function setminPrice(uint256 newPrice) public {
     require(msg.sender == manager,"only manager can reach  here");
     minbetAM = newPrice;
+  }
+
+  function setrate(uint256 newrate) public {
+    require(msg.sender == manager,"only manager can reach  here");
+    winrate = newrate;
   }
 
 
